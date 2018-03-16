1877aad76af6bbc64e6cba23f922c46b1662f9f8
Your Name <you@example.com>
2018-03-15 23:06:29 -0400

    Added object for input of student and class

--- src/main/java/com/scheduling/system/Domain/Dtos/ClassDto.java
+++ src/main/java/com/scheduling/system/Domain/Dtos/ClassDto.java
@@ -1,7 +1,37 @@
 package com.scheduling.system.Domain.Dtos;
 
 /**
+ *
+ * This ClassDto class is for define attributes of input for a Class.
+ *
  * Created by Rafael Encinas.
  */
 public class ClassDto {
+    private String code;
+    private String title;
+    private String description;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
--- src/main/java/com/scheduling/system/Domain/Dtos/StudentDto.java
+++ src/main/java/com/scheduling/system/Domain/Dtos/StudentDto.java
@@ -1,7 +1,28 @@
 package com.scheduling.system.Domain.Dtos;
 
 /**
+ *
+ * This StudentDto class is for define attributes of input for a Student.
+ *
  * Created by Rafael Encinas.
  */
 public class StudentDto {
+    private String name;
+    private String last_name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
 }

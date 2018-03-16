bd691c11632ab514767ade579b62aec2ab187aa1
Your Name <you@example.com>
2018-03-15 21:50:35 -0400

    Initialized super simple scheduling system project

---
+++ src/main/java/com/scheduling/system/ApiSuperSimpleSchedulingSystemApplication.java
@@ -0,0 +1,12 @@
+package com.scheduling.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSuperSimpleSchedulingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSuperSimpleSchedulingSystemApplication.class, args);
+	}
+}

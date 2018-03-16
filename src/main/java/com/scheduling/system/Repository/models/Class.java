14d03ac534b5ea68555fbae3b36fcce10aaac381
Your Name <you@example.com>
2018-03-15 23:17:06 -0400

    Added attributes on models for student and Class

--- src/main/java/com/scheduling/system/Repository/models/Class.java
+++ src/main/java/com/scheduling/system/Repository/models/Class.java
@@ -1,7 +1,49 @@
 package com.scheduling.system.Repository.models;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * Created by Rafael Encinas.
  */
 public class Class {
+    public Integer id;
+    public String code;
+    public String title;
+    public String description;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
--- src/main/java/com/scheduling/system/Repository/models/Student.java
+++ src/main/java/com/scheduling/system/Repository/models/Student.java
@@ -1,7 +1,44 @@
 package com.scheduling.system.Repository.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * Created by Rafael Encinas.
  */
+
+@Entity
 public class Student {
+
+    private Integer id;
+    private String name;
+    private String last_name;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
 }
